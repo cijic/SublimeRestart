e78a964a824839d5b1c86948dc1d522a2f4fb01b
cijic <cijic@users.noreply.github.com>
2023-06-22 20:27:50 +0300

    Update restart.py
    - Added fix for restarting 'cause it's wasn't restarted, it's just killed Sublime;
    - PEP8 format;

--- restart.py
+++ restart.py
@@ -1,27 +1,33 @@
-import sublime, sublime_plugin, os, sys, subprocess
+#!/usr/bin/env python3
+
+import sublime
+import sublime_plugin
+import os
+import sys
+import subprocess
 
 __author__ = 'Hassen Ben Yeddder'
 __email__ = 'hassenbenyedder@gmail.com'
 
+
 class RestartCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         path = sublime.load_settings("Restart.sublime-settings").get("path")
-        
+
         if sys.platform == 'win32':
-            if sublime.version()[:1]=='3':
-                subprocess.call('taskkill /im sublime_text.exe /f && cmd /C "'+ os.path.join(os.getcwd(), 'sublime_text.exe') + '"', shell=True)
-            else:
-                os.execl(sys.executable,' ')
+            subprocess.call('taskkill /im sublime_text.exe /f && cmd /C "' +
+                            os.path.join(os.getcwd(), 'sublime_text.exe') + '"', shell=True)
         elif sys.platform == 'darwin':
-            #Restarting ST3 on mac
-            if sublime.version()[:1]=='3':
-                subprocess.call("pkill subl && "+ os.path.join(os.getcwd(), 'subl'), shell=True)
+            # Restarting ST3 on mac
+            if sublime.version()[:1] == '3':
+                subprocess.call("pkill subl && " +
+                                os.path.join(os.getcwd(), 'subl'), shell=True)
             else:
                 os.execl(os.path.join(os.getcwd(), 'subl'))
         else:
-            #Restarting ST3 on linux
-            if sublime.version()[:1]=='3':
-                subprocess.call("pkill 'sublime_text' && "+ os.path.join(os.getcwd(), 'sublime_text'), shell=True)
-                
+            # Restarting ST3 on linux
+            if sublime.version()[:1] == '3':
+                subprocess.call("pkill 'sublime_text' && " +
+                                os.path.join(os.getcwd(), 'sublime_text'), shell=True)
             else:
                 os.execl(os.path.join(os.getcwd(), 'sublime_text'))
